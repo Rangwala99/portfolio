2a2434d9e6bac5bcd70c2fc8e08bc8708e17b93b
Devesh Anand <ananddeves22@gmail.com>
2020-04-10 23:52:35 +0530

    ui

--- js/main.js
+++ js/main.js
@@ -70,23 +70,23 @@ $(document).ready(function($) {
 	};
 	fullHeight();
 
-	var burgerMenu = function() {
+	// var burgerMenu = function() {
 
-		$('.js-colorlib-nav-toggle').on('click', function(event) {
-			event.preventDefault();
-			var $this = $(this);
-			if( $('body').hasClass('menu-show') ) {
-				$('body').removeClass('menu-show');
-				$('#colorlib-main-nav > .js-colorlib-nav-toggle').removeClass('show');
-			} else {
-				$('body').addClass('menu-show');
-				setTimeout(function(){
-					$('#colorlib-main-nav > .js-colorlib-nav-toggle').addClass('show');
-				}, 900);
-			}
-		})
-	};
-	burgerMenu();
+	// 	$('.js-colorlib-nav-toggle').on('click', function(event) {
+	// 		event.preventDefault();
+	// 		var $this = $(this);
+	// 		if( $('body').hasClass('menu-show') ) {
+	// 			$('body').removeClass('menu-show');
+	// 			$('#colorlib-main-nav > .js-colorlib-nav-toggle').removeClass('show');
+	// 		} else {
+	// 			$('body').addClass('menu-show');
+	// 			setTimeout(function(){
+	// 				$('#colorlib-main-nav > .js-colorlib-nav-toggle').addClass('show');
+	// 			}, 900);
+	// 		}
+	// 	})
+	// };
+	// burgerMenu();
 	
 	var counter = function() {
 		
